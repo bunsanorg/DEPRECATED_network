36888ca478d01dd813aa95a5f1c6801597edba16
Aleksey Filippov <sarum9in@gmail.com>
2012-11-08 04:09:00 +0400

    bunsan::network project separated from bunsan::common.

---
+++ src/base64.cpp
@@ -0,0 +1,20 @@
+#include "bunsan/base64.hpp"
+
+#include <xmlrpc-c/base64.hpp>
+
+std::string bunsan::base64::encode(const std::vector<unsigned char> &data)
+{
+    return xmlrpc_c::base64FromBytes(data);
+}
+
+std::string bunsan::base64::encode(const std::string &data)
+{
+    std::vector<unsigned char> data_(data.begin(), data.end());
+    return encode(data_);
+}
+
+std::vector<unsigned char> bunsan::base64::decode(const std::string &base64)
+{
+    return xmlrpc_c::bytesFromBase64(base64);
+}
+
